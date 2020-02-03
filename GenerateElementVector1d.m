f1ed2db094ebb2827bbb48cf11b8f4370e923c02
asrono <38725348+asrono@users.noreply.github.com>
2020-02-03 13:33:12 +0100

    update plotting

--- GenerateElementVector1d.m
+++ GenerateElementVector1d.m
@@ -2,4 +2,6 @@
 % This routine will create the element matrix
 
 %% Implementation
-f_elem = h/2*[1, 1]';
+f_elem = zeros(n-1,2);
+f_elem(:,1) = h/2*fun(x(elmat(:,1)));
+f_elem(:,2) = h/2*fun(x(elmat(:,2)));
--- Preamble.m
+++ Preamble.m
@@ -2,7 +2,7 @@
 % This file will setup the necessary settings
 
 %% Implementation
-close all; clear all;
+close all; clear all; clc;
 set(0,'defaulttextinterpreter','latex');
 set(0,'defaultaxesfontsize',14);
 set(0,'defaultaxesticklabelinterpreter','latex');
