1061229d0cc8ce13266e2f15b147f5d8565ae6a2
asrono <38725348+asrono@users.noreply.github.com>
2020-02-12 22:25:43 +0100

    automate refinements

--- 2D/AP3001Comp.m
+++ 2D/AP3001Comp.m
@@ -1,10 +1,7 @@
 % Construction of linear problem
-
 BuildMatricesandVectors;
 
 % Solution of linear problem
-
 u = S \ f;
-
 v_x = M \ (C_x*u);
 v_y = M \ (C_y*u);
--- 2D/Main.m
+++ 2D/Main.m
@@ -1,6 +1,5 @@
 %% femsolve1d.m
 % This file will assemble an FEM problem and compute the solution
-
 Preamble;
 
 %% Input variables
@@ -10,18 +9,17 @@ mu  = 1.002e-3; % [Pa s]
 K   = 10;       % [m/s]
 ph  = 1e6;      % [Pa]
 
-x_domain = [-1, 1];     % [m]
-y_domain = [-1, 1];     % [m]
-
 % Bool for setting compatibility
 comp = false;
-K = 10;
-%% wells
+
+% Define domain and well
+x_domain = [-1, 1];     % [m]
+y_domain = [-1, 1];     % [m]
 x_vec_well = [0.6*cos(2/5*pi*((1:5)-1)),0;
               0.6*sin(2/5*pi*((1:5)-1)),0];
 
+number_of_refinements = 2;
 %% Run main program
 AP3001Mesh;
-BuildMatricesandVectors;
 AP3001Comp;
 AP3001Post;
